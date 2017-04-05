27459f6dd0c67e249dc8ce127194a118a9fc2753
otofu-square <yukita@feedforce.jp>
2017-04-05 21:45:55 +0900

    :+1: Passing arguments

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const { buildSchema } = require('graphql')
 
 const schema = buildSchema(`
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
+    rollThreeDice(numDice: Int!, numSides: Int): [Int]
   }
 `)
 
 const root = {
-  quoteOfTheDay: () => Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within',
-  random: () => Math.random(),
-  rollThreeDice: [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6)),
+  rollThreeDice: (args) => {
+    const output = []
+    for (let i = 0; i < args.numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (args.numSides || 6)))
+    }
+    return output
+  },
 }
 
 const app = express()
