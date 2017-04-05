eea56950093fe38651db0932037bbfcd48359309
otofu-square <yukita@feedforce.jp>
2017-04-05 21:35:59 +0900

    :+1: Use basic types

--- server.js
+++ server.js
@@ -4,12 +4,16 @@ const { buildSchema } = require('graphql')
 
 const schema = buildSchema(`
   type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `)
 
 const root = {
-  hello: () => 'Hello World'
+  quoteOfTheDay: () => Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within',
+  random: () => Math.random(),
+  rollThreeDice: [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6)),
 }
 
 const app = express()
