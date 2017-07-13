5460f243e698005a576316c3fc2c741eea18e67c
otofu-square <yukita@feedforce.jp>
2017-07-14 00:10:57 +0900

    :wrench: Add config.ts

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import * as bodyParser from 'body-parser';
 import { graphqlExpress } from 'graphql-server-express';
 
 import schema from './schema';
+import { PORT } from './config';
 
 const app = express();
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Hackernews GraphQL server running on port ${PORT}.`);
 });
