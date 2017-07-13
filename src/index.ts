767bf6975d49b3b6ac09e771253366575b58fd3e
otofu-square <yukita@feedforce.jp>
2017-07-13 23:59:54 +0900

    :+1: Add graphql backend

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import { graphqlExpress } from 'graphql-server-express';
+
+import schema from './schema';
+
+const app = express();
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Hackernews GraphQL server running on port ${PORT}.`);
+});
