6481ad1a071400d9c420bab24164f08e44be2193
otofu-square <yukita@feedforce.jp>
2017-12-11 23:57:58 +0900

    :wrench: Improve config-overrides.js

--- how-to-graphql/config-overrides.js
+++ how-to-graphql/config-overrides.js
@@ -1,6 +1,24 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const tsImportPluginFactory = require('ts-import-plugin');
+const { getLoader } = require('react-app-rewired');
 
-module.exports = function override(config, env) {
-  config = injectBabelPlugin('emotion/babel', config);
+module.exports = (config, env) => {
+  const tsLoader = getLoader(
+    config.module.rules,
+    rule =>
+      rule.loader &&
+      typeof rule.loader === 'string' &&
+      rule.loader.includes('ts-loader'),
+  );
+  tsLoader.options = {
+    getCustomTransformers: () => ({
+      before: [
+        tsImportPluginFactory({
+          libraryName: 'antd',
+          libraryDirectory: 'es',
+          style: 'css',
+        }),
+      ],
+    }),
+  };
   return config;
 };
