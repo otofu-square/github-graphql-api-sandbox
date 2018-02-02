a5a7849f23e59d8cd2b7cb2ea396ee52388c8dd1
otofu-square <yukita@feedforce.jp>
2018-02-02 23:38:45 +0900

    :tada: Add appolo binding to index.ts

--- how-to-graphql/src/index.tsx
+++ how-to-graphql/src/index.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { App } from './App';
+import { GRAPHQL_ENDPOINT } from './utils/urls';
 import './globalStyles';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+const httpLink = createHttpLink({ uri: GRAPHQL_ENDPOINT });
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root') as HTMLElement,
+);
 registerServiceWorker();
